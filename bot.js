b45bd982a7d93189b04dc51c53ea9cf3af03bc08
JnabY <43906195+jnabyiq@users.noreply.github.com>
2018-12-18 10:09:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ client.on('guildMemberAdd',async member => {
 
 
 client.on('guildMemberAdd',async member => {
-  if(member.guild.id !== '518756105166520320') return;
+  if(member.guild.id !== '462347265747451914') return;
   setTimeout(function(){
   member.guild.channels.find(r => r.id === '524541102125088798').send('You`re in **Bubbles** Avatar welcome.');
 },6000);
