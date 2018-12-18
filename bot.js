16e6865b53780f80993bed9bbc67c7356e0c63d1
JnabY <43906195+jnabyiq@users.noreply.github.com>
2018-12-18 04:52:45 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,6 +47,15 @@ console.log("Failed To Join:\n The Channel Type isn't \"text\"");
  
 
 client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '462347265747451914') return;
+  setTimeout(function(){
+  member.guild.channels.find(r => r.id === '524541102125088798').send('**welçome you’re in BubblesAvatar**.');
+},6000);
+});
+
+
+
+client.on('guildMemberAdd',async member => {
   if(member.guild.id !== '518756105166520320') return;
   setTimeout(function(){
   member.guild.channels.find(r => r.id === '521374016036929536').send('Welcome to **Point** .');
