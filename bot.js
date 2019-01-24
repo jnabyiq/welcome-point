a19e247fe6dc21980ae20eb0d09f3971891f2c69
JnabY <43906195+jnabyiq@users.noreply.github.com>
2019-01-24 04:46:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,20 @@ client.on('ready', () => {
 
 });
 
+client.on('message', msg => {
 
+    if (msg.content == '$$$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("536113868884148234").join(); 
+    });
 
 client.on('guildMemberAdd',async member => {
   if(member.guild.id !== '527036248984911890') return;
